95da50a066548010dba28716c7197bec431e7d5e
Ethan Lourens <52194238+EthanLourens@users.noreply.github.com>
2021-12-11 20:18:50 +0200

    Update Ethan_Lourens_BE_Coding_Test.java

--- Ethan_Lourens_BE_Coding_Test.java
+++ Ethan_Lourens_BE_Coding_Test.java
@@ -13,12 +13,7 @@ public class Ethan_Lourens_BE_Coding_Test {
 
     public static String removeAllDigit(String str) {
         String result = "";
-
-        // Traverse the String from start to end
         for (int i = 0; i < str.length(); i++) {
-
-            // Check if the specified character is not digit
-            // then add this character into result variable
             if (!Character.isDigit(str.charAt(i))) {
                 result = result + str.charAt(i);
             }
